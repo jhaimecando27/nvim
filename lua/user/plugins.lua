61b16d5158c305d2a042e825604ca4a162107822
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-03-09 12:43:58 +0800

    Add Vimwiki

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,6 +58,7 @@ return packer.startup(function(use)
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
   use "folke/which-key.nvim"
   use "preservim/tagbar"
+  use "vimwiki/vimwiki" -- Just use autoformat
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
