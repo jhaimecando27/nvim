5c56fd40b6047007f23e5b301cae1dfbbbc0e874
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-04-21 00:16:31 +0800

    Add line break

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -32,6 +32,7 @@ local options = {
   wrap = false,                            -- display lines as one long line
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
+  linebreak = true,
   guifont = "monospace:h17",               -- the font used in graphical neovim applications
   cc = "70"
 }
