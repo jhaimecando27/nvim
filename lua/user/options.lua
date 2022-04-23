76ea4cf44ae7424f3300b7d85dd94ef5bb4bde4c
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2022-04-23 14:45:27 +0800

    decrease font

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -33,7 +33,7 @@ local options = {
   scrolloff = 8,                           -- is one of my fav
   sidescrolloff = 8,
   linebreak = true,
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
+  guifont = "monospace:h13",               -- the font used in graphical neovim applications
   cc = "70"
 }
 
