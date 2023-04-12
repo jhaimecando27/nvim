767cdb7f901f41f6fa58227aa7647be38d7533db
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2023-04-12 12:10:01 +0800

    feat(null-ls): add more formatter

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,17 +1,18 @@
 local null_ls_ok, null_ls = pcall(require, "null_ls")
 if not null_ls_ok then
-	return
+    return
 end
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
-null_ls.setup({
-	sources = {
-		formatting.stylua,
-		formatting.prettier,
-		formatting.black,
+null_ls.setup {
+    sources = {
+        formatting.prettier,
+        formatting.black,
+        formatting.stylua,
+        formatting.google_java_format,
 
-		diagnostics.flak8,
-	},
-})
+        diagnostics.flake8,
+    },
+}
