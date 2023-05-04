688d10456e45f8d546d06f4abf17e5cb25193083
Jhaime Cando <jhaimecando27@gmail.com>
2023-05-04 17:00:12 +0800

    feat(plugins): add gitsigns

--- lua/jhaime/packer.lua
+++ lua/jhaime/packer.lua
@@ -1,76 +1,78 @@
 local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-    -- Packer can manage itself
-    use("wbthomason/packer.nvim")
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    -- File navigation
-    use({
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
-        requires = { { "nvim-lua/plenary.nvim" } },
-    })
+	-- File navigation
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    -- Code highlighter
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-    use 'nvim-treesitter/nvim-treesitter-context'
+	-- Code highlighter
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/nvim-treesitter-context")
 
-    -- LSP
-    use({
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v1.x",
-        requires = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
+	-- LSP
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
-        },
-    })
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-    -- Inject LSP diagnostics, code actions, and more via Lua
-    use("jose-elias-alvarez/null-ls.nvim")
+	-- Inject LSP diagnostics, code actions, and more via Lua
+	use("jose-elias-alvarez/null-ls.nvim")
 
-    -- Others
-    use("folke/zen-mode.nvim")
+	-- Others
+	use("folke/zen-mode.nvim")
 
-    -- Used for markdown completion
-    use("vimwiki/vimwiki")
+	-- Used for markdown completion
+	use("vimwiki/vimwiki")
 
-    -- Metrics about your programming
-    use("wakatime/vim-wakatime")
+	-- Metrics about your programming
+	use("wakatime/vim-wakatime")
 
-    -- Colorscheme
-    use({ "catppuccin/nvim", as = "catppuccin" })
+	-- Colorscheme
+	use({ "catppuccin/nvim", as = "catppuccin" })
 
-    -- CoPilot
-    use("github/copilot.vim")
+	-- CoPilot
+	use("github/copilot.vim")
 
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+	use("lewis6991/gitsigns.nvim")
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
