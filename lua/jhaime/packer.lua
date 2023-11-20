8312234911c28036f06202bcfbbdcd9de5e856be
Jhaime Cando <74945153+jhaimecando27@users.noreply.github.com>
2023-11-21 00:11:07 +0800

    fix(telescope): crashes when used
    > THIS IS ONLY A WORKAROUND
    
    - Removes the tag. It will be using updated version of telescope.

--- lua/jhaime/packer.lua
+++ lua/jhaime/packer.lua
@@ -18,7 +18,6 @@ return require("packer").startup(function(use)
 	-- File navigation
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
